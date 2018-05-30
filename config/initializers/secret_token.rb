c364f0dc9281541fb4b86626788846747d396689
Yadagiri Tandu <yadagiri.tandu@adroitent.com>
2018-05-30 22:58:32 +0530

    adds all th eproject files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitDemo::Application.config.secret_token = 'c2901b28a3cbd924ed12acdcb662270d839064c51656a71609ce170e85c7d453baee82cb91e48a11a87ff47556c7140c138d3e72b797304b32284d856b5c6964'
